7c27738d3e88f470140b590e70bdca328ffa70d7
DiegoPertierraM <diegopmenendez@gmail.com>
2024-02-16 16:58:21 +0100

    Added generatePiramid function

---
+++ helpers.js
@@ -0,0 +1,30 @@
+// -- 1
+
+export const generatePiramid = (height) => {
+  let piramid = '*';
+  for (let i = 1; i < height; i++) {
+    console.log(piramid);
+    piramid += '**';
+  }
+  return piramid;
+};
+
+// -- 2
+
+// -- 3
+
+// -- 4
+
+// -- 5
+
+// -- 6
+
+// -- 7
+
+// -- 8
+
+// -- 9
+
+// -- 10
+
+// -- 11
---
+++ index.js
@@ -0,0 +1,3 @@
+import { generatePiramid } from './helpers.js';
+
+console.log(generatePiramid(7));
